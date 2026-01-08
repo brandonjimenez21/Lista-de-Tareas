d93c0d239df8fef6e42873eda642922e406ab63d
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2026-01-07 22:58:42 -0500

    backup cors

--- mp1-backend/api/index.js
+++ mp1-backend/api/index.js
@@ -30,18 +30,12 @@ const allowedOrigins = [
   "http://localhost:5173",
 ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error("CORS not allowed"));
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
